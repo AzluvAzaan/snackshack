97cf696cdeac4a63c5da51807c349546eaa28282
doryfishh <sitingdorothy@gmail.com>
2024-10-24 18:16:17 +0800

    review button ver1

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Home from '@/views/Home.vue';
 import Map from '@/views/Map.vue';
 import Wheel from '@/views/Wheel.vue';
 import Test from '@/views/Test.vue';
+import Review from '@/views/Review.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/map', name: 'Map', component: Map },
   { path: '/wheel', name: 'Wheel', component: Wheel },
   { path: '/test', name: 'Test', component: Test },
+  { path: '/review', name: 'Review', component: Review },
 ];
 
 const router = createRouter({
