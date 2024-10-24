a81d0e8ec43a6188e867eba034826cb3fe16f091
AzluvAzaan <azluv.azaan.2023@scis.smu.edu.sg>
2024-10-24 19:12:16 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import Home from '@/views/Home.vue';
 import Map from '@/views/Map.vue';
 import Wheel from '@/views/Wheel.vue';
-import Test from '@/views/Test.vue';
 import Review from '@/views/Review.vue';
 import Login from '@/views/Login.vue';
 import Admin from '@/views/Admin.vue';
@@ -13,28 +10,10 @@ const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/map', name: 'Map', component: Map },
   { path: '/wheel', name: 'Wheel', component: Wheel },
-  { path: '/test', name: 'Test', component: Test },
   { path: '/review', name: 'Review', component: Review },
-=======
-=======
->>>>>>> Stashed changes
-import Home from './views/Home.vue';
-import Review from './components/review.vue';
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/review',
-    name: 'Review',
-    component: Review,
-  },
   { path: '/login', name: 'Login', component: Login },
   { path: '/admin', name: 'Admin', component: Admin },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(),
